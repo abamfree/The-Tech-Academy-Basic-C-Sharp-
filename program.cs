d14a0b6e469f9c6cdede219c32841045adeb7506
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-12 15:08:53 -0700

    initialcommit
    AccountingPage3C#

---
+++ program.cs
@@ -0,0 +1,19 @@
+using system;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
+}
