d3f1e29c7402197bc63eae257164737add595f76
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-12 10:35:58 -0700

    C:Users/abraham/Source/repos/Act/Act.sin
    Page34 C#

--- program.cs
+++ program.cs
@@ -1,7 +1,19 @@
 using system;
-class program {
-	static void Main()
-	{
-		Console.WriteLine("Hello World!")
-	}
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
 }
