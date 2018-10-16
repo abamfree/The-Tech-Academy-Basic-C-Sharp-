786d35769e169e83ed6de4deaebc4645cc3823d1
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-15 19:32:42 -0700

    initial commit
    Car Insurance page 75 C#

---
+++ CarInsurance/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int age1 = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI and please type true or false?");
+            string DUI = Console.ReadLine();
+            bool correct = Convert.ToBoolean(DUI);
+           Console.WriteLine("Let's go on you might be Qualified");
+            Console.WriteLine("How many speeding tickets do you have?");
+            string noOfTickets = Console.ReadLine();
+            int noOfTickets1 = Convert.ToInt32(noOfTickets);
+
+            if (age1 > 15 && noOfTickets1 < 3 && correct==false)
+            {
+                Console.WriteLine(" Congrats you're Qualified");
+            }
+            else
+            {
+                Console.WriteLine("SORRY!");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
