c1e7712f2b67091c42c89d4ca420873299b10c57
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-16 17:38:09 -0700

    initial commit
    C# page 94 do and while Boolean comparison

---
+++ ComparisonWhile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonWhile
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string name = "abraham";
+            int i = 0;
+            bool boolVariable = false;
+            while (i < 5) 
+            {
+                Console.WriteLine("What is your name");
+                string userInput = Console.ReadLine();
+                if (name == userInput)
+                {
+                    Console.WriteLine("Congratz");
+                    boolVariable = true;
+                    break;
+                }
+                else if (name != userInput)
+                {
+                    Console.WriteLine("try again");
+                    i = i + 1;
+                }
+                if (i == 3 && boolVariable == false)
+                {
+                    Console.WriteLine("BINGO!");
+                    break;
+                }
+            }
+            
+            Console.ReadLine();
+        }
+
+    }
+
+}
+
+
+
+
+
+
---
+++ DoWhileBoolean/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileBoolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            string name = "abraham";
+            int i = 0;
+            bool boolVariable = false;
+            do
+            {
+                Console.WriteLine("What is your name");
+                string userInput = Console.ReadLine();
+                if (name == userInput)
+                {
+                    Console.WriteLine("Congratz");
+                    boolVariable = true;
+                    break;
+                }
+                else if (name != userInput)
+                {
+                    Console.WriteLine("try again");
+                    i = i + 1;
+                }
+               if (i ==3 && boolVariable == false)
+                {
+                    Console.WriteLine("BINGO!");
+                    break;
+                }
+            }
+            while (i < 5);
+            Console.ReadLine();
+        } 
+
+            
+    }
+    }
+
