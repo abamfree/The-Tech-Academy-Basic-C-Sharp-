58525808a9995bc66841ccfd5e776cace4872e34
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-19 15:02:19 -0700

    initial Comiit
    Create a class with 3 methods and do some math operation

---
+++ Classwith3Methods/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwith3Methods
+{
+    class Class1
+    {
+         
+
+    public static void Method1(int num01)
+
+        {
+
+            Console.WriteLine("Your number was added to 10 resulting in: " + (num01 + 10));
+
+        }
+
+        public static void Method2(int num01)
+
+        {
+
+            Console.WriteLine("Your number was multiplied by 10 resulting in: " + num01 * 10);
+
+        }
+
+        public static void Method3(int num01)
+
+        {
+
+            Console.WriteLine("Your number was divided by 10 resulting in: " + num01 / 10);
+
+        }
+
+    }
+
+}
+  
