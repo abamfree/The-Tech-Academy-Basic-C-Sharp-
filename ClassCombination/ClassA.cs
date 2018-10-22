bfe14cbf9bc0bfaa0d175a218ca935e5e2e86b52
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-21 21:33:34 -0700

    initialcommit
    Class Combination C#

---
+++ ClassCombination/ClassA.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCombination
+{
+    class ClassA
+    {
+
+
+        public int Method1(int num01, int num02 = 0)
+
+        {
+
+            // 1. Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return a integer result.
+
+            int result = num01 + num02;
+
+            return result;
+        }
+    }
+}
---
+++ ClassCombination/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. In the Main() method of the console app, instantiate the class.
+
+            ClassA addition = new ClassA();
+
+
+
+            //3. Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
+
+            Console.WriteLine("Please enter a number.");
+
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("If you would like, please enter in another number.");
+
+            string userNum2 = Console.ReadLine();
+
+
+
+            //4. Call the method in the class, passing in the one or two numbers entered.
+
+            if (userNum2 == "")
+
+            {
+
+                int result1 = addition.Method1(userNum1);
+
+                Console.WriteLine(userNum1 + " + 0 = " + result1);
+
+                Console.ReadLine();
+
+            }
+
+            else
+
+            {
+
+                int userNumber2 = Convert.ToInt32(userNum2);
+
+                int result2 = addition.Method1(userNum1, userNumber2);
+
+                Console.WriteLine(userNum1 + " + " + userNumber2 + " = " + result2);
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
