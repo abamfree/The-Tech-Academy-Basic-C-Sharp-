0c10236ba4e984a7bfa612244e5df6403f6763d0
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-22 15:28:29 -0700

    Final commit
    page 111 C sharp

---
+++ Classwith3MethodsNew/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwith3MethodsNew
+{
+    class Class1
+    {
+        public int Method1(int num01)
+
+        {
+            int result = num01 + 20;
+            //Console.WriteLine("Your number was added to 10 resulting in: " + (num01 + 10));
+            return result;
+        }
+
+        public int Method2(int num01)
+
+        {
+            int result = num01 * 10;
+            return result;
+
+            //Console.WriteLine("Your number was multiplied by 10 resulting in: " + num01 * 10);
+
+        }
+
+        public decimal Method3(decimal num01)
+
+        {
+            decimal result = num01 / 10;
+            return result;
+
+            // Console.WriteLine("Your number was divided by 10 resulting in: " + num01 / 10);
+
+        }
+
+    }
+
+}
+  
+ 
---
+++ Classwith3MethodsNew/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classwith3MethodsNew
+{
+    class Program
+    {
+      
+            static void Main(string[] args)
+            {
+                Class1 addition = new Class1();
+                Console.WriteLine("Please enter a number to have math operations performed on it.");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                int result = addition.Method1(num1);
+                Console.WriteLine(result);
+                //Console.ReadLine();
+
+            Class1 multiplication = new Class1();
+            Console.WriteLine("Please enter a number to have math operations performed on it.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result2 = multiplication.Method2(num2);
+            Console.WriteLine(result2);
+            // Console.ReadLine();
+
+            Class1 division = new Class1();
+            Console.WriteLine("Please enter a number to have math operations performed on it.");
+            decimal num3 = Convert.ToDecimal(Console.ReadLine());
+            decimal result3 = division.Method3(num3);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
