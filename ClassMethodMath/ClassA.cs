a621676ae246540f93814737f912dca721f47594
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-21 21:19:29 -0700

    Initial commit
    class with 3 methods C#

---
+++ ClassMethodMath/ClassA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodMath
+{
+    class ClassA
+    {
+        // 1. Create a class. In that class, create a method that will take in an integer, do a math operation to it and then return the answer as an integer.
+
+        public int Method1(int num01)
+
+        {
+
+            int result = num01 + 20;
+
+            return result;
+
+        }
+
+
+
+        //3. Add a second method to the class, with the same name, that will take in a decimal, do a different math operation to it and then return the answer as an integer.
+
+        public decimal Method1(decimal num01)
+
+        {
+
+            decimal result = num01 / 10;
+
+            return result;
+
+        }
+
+
+
+        //5. Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it and then return the answer as an integer.
+
+        public int Method1(string num01)
+
+        {
+
+            int result = Convert.ToInt32(num01);
+
+            result = result * 5;
+
+            return result;
+        }
+    }
+}
---
+++ ClassMethodMath/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.In the Main() method of the console app, instantiate the class and call the one method, passing in an integer, such as 12. Then display the result to the screen.
+
+            ClassA addition = new ClassA();
+
+            int result1 = addition.Method1(12);
+
+            Console.WriteLine(result1);
+
+            Console.ReadLine();
+
+
+
+            //4. In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+
+            ClassA division = new ClassA();
+
+            decimal result2 = division.Method1(.5M);
+
+            Console.WriteLine(result2);
+
+            Console.ReadLine();
+
+
+
+            //6. In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.
+
+            ClassA multiplication = new ClassA();
+
+            int result3 = multiplication.Method1(10);
+
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
