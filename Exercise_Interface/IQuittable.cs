fb24a057f1231d92ec49ea455379c50529492fd3
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-23 13:08:29 -0700

    InitialCommit
    Interface Exercise page 126 C#

---
+++ Exercise_Interface/IQuittable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Interface
+{
+    interface IQuittable
+    {
+        //1. create an interface and have it define a void method call Quit
+
+        void Quit();
+
+    }
+}
---
+++ Exercise_Interface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Use polymorphism to create an object of type IQuittable and call 
+            //the Quit() method on it. Hint: an object can be of an 
+            //interface type if it implements that specific interface.
+
+            IQuittable employee = new Employee();
+
+              employee.Quit();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
