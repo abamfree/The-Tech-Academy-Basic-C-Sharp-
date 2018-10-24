dddd164da987431c998ed8f27522ffbe53d64df1
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-23 21:32:38 -0700

    initialcommit
    assignment of Struct

---
+++ Exercise_struct/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_struct
+{
+    //1. Create a struct called Number and give it the property "Amount" and have it be of data type decimal.
+    public struct Number
+
+        {
+        public decimal Amount { get; set; }
+
+        }
+
+    }
+
---
+++ Exercise_struct/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+
+            Number number = new Number();
+
+            number.Amount = 503;
+        //3. Print this amount to the console.
+
+            Console.WriteLine(" Here you got an amount of\n " + number.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
