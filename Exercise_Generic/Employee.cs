68d061201ed837bcaa5931cd09e5a10dc2ead506
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-25 04:21:39 -0700

    initialcommit
    Generic c sharp

---
+++ Exercise_Generic/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Generic
+{
+    
+    
+        //1. Make the Employee class take a generic type parameter.
+
+        class Employee<T> : Person
+
+        {
+
+        //2. Add a property to the Employee class called "things" and 
+        //have its data type be a generic list matching the generic type of the class.
+
+            public List<T> Things { get; set; }
+
+
+
+            public int Id { get; set; }
+
+
+
+            public new void SayName()
+
+            {
+
+                base.SayName();//
+
+            }
+
+            public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+
+            {
+
+                if (employee1.Id == employee2.Id)
+
+                {
+
+
+
+                    return true;
+
+
+
+                }
+
+                else return false;
+
+
+
+
+
+            }
+
+            public static bool operator !=(Employee<T> employee1, Employee<T> employee2)
+
+            {
+
+                if (employee1.Id != employee2.Id)
+
+                {
+
+                    return false;
+
+                }
+
+                else return true;
+
+            }
+
+
+
+            public override int GetHashCode()
+
+            {
+
+                return base.GetHashCode();
+
+            }
+
+
+
+            public override bool Equals(object obj)
+
+            {
+
+                return base.Equals(obj);
+
+
+
+            }
+        }
+}
---
+++ Exercise_Generic/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Instantiate an Employee object with type "string" as its generic parameter. 
+            //Assign a list of strings as the property value of Things.
+
+            Employee<string> EmployeeString = new Employee<string>();
+
+            EmployeeString.Things = new List<string>();
+
+            EmployeeString.Things.Add("xxx");
+
+            EmployeeString.Things.Add("yyy");
+
+            EmployeeString.Things.Add("zzz");
+
+
+
+            //4. Instantiate an Employee object with type "int" as its generic parameter. Assign a list of integers as the property value of Things.
+
+            Employee<int> EmployeeInt = new Employee<int>();
+
+            EmployeeInt.Things = new List<int>();
+
+            EmployeeInt.Things.Add(5);
+
+            EmployeeInt.Things.Add(6);
+
+            EmployeeInt.Things.Add(7);
+
+
+
+            //5. Create a loop that prints all of the Things to the Console.
+
+            foreach (string thing in EmployeeString.Things)
+
+            {
+
+                Console.WriteLine(thing);
+
+
+
+            }
+
+
+
+            foreach (int thing in EmployeeInt.Things)
+
+            {
+
+                Console.WriteLine(thing);
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+                   }
+    }
+}
