65f97aecaaab77df9b009fbfd609665c98a88d04
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-26 15:16:56 -0700

    intialcommit
    Date time and add number as an hour

---
+++ DateTime/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+                //1.Prints the current date and time to the console.
+
+                DateTime currentDateTime = DateTime.Now;
+
+                Console.WriteLine(currentDateTime);
+
+
+
+                //2.Asks the user for a number.
+
+                Console.WriteLine("Please enter in a number");
+
+                double userNumber = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
+                //3.Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+
+
+
+      Console.WriteLine("The time will be {0} after adding {1} as number of hours.", currentDateTime.AddHours(userNumber), userNumber);
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+
