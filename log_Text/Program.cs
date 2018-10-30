57607dd81372eff6c1b0cd8c785eec2ac157e55f
Abraham <42303446+abamfree@users.noreply.github.com>
2018-10-30 14:44:17 -0700

    intialcommit
    how to R/W a file to and from a text file

---
+++ log_Text/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace log_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1. Ask a user for a number.
+
+            Console.WriteLine("Please enter a number.");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+
+            //2. Log that number to a text file.
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Abraham\source\repos\log_Text\log.txt", true))
+            {
+                file.WriteLine(userNumber);
+            }
+            //3. Print the text file back to the user.
+
+            using (StreamReader file = new StreamReader(@"C:\Users\Abraham\source\repos\log_Text\log.txt"))
+
+            {
+
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+
+                {
+
+                    Console.WriteLine("Here is the number you entered: " + line);
+
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+              
